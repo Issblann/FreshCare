4ffeb045b0b97c159c9d775abfd4143b6f0fea9d
issblann <anaisabelpp@outlook.com>
2023-01-16 15:40:05 -0500

    agrego funcion de carrito renderizar productos

--- assets/main.js
+++ assets/main.js
@@ -9,7 +9,8 @@ const buttonCategory = document.querySelectorAll(".buttonCategory");
 const productsContainer = document.querySelector(".products-container");
 // boton añadir mas
 const btnAddMore = document.querySelector(".verMas");
-
+// boton agregar al carrito
+const btnAddToCart = document.querySelector(".btn-add");
 // boton user
 const btnUser = document.querySelector(".user-icon");
 // boton menu
@@ -22,8 +23,20 @@ const userMenu = document.querySelector(".user-container");
 const menuList = document.querySelector(".navbar-list");
 // carrito contenedor
 const cart = document.querySelector(".cart-container");
+// burbuja del carrito
+const bubbleCart = document.querySelector(".cart-bubble");
+// contenedor de las cards del carrito
+const cardContainer = document.querySelector(".card-container");
+// precio total en carrito
+const priceTotal = document.querySelector(".total");
+// boton comprar ahora
+const btnBuyCart = document.querySelector("#btn-cart-buy");
+// boton vaciar carrito
+const btnCleanCart = document.querySelector("#btn-cart-clear");
 // overlay
 const overlay = document.querySelector(".overlay");
+// successModal para el mensaje
+const successModal = document.querySelector(".add-modal");
 
 // Crear localStorage del carrito
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
@@ -50,7 +63,7 @@ const renderProduct = (product) => {
         <div class="product-price">
           <p><span>$</span>${price}</p>
         </div>
-        <div><button class="btn-add" data-id="${id}" data-name="${name}" data-img="${cardImg}" >Agregar al carrito</button></div>
+        <div><button class="btn-add" data-id="${id}" data-name="${name}" data-price="${price}"  data-img="${cardImg}" >Agregar al carrito</button></div>
       </div>
     </div>
   </div>
@@ -157,6 +170,8 @@ const toggleCart = () => {
   overlay.classList.toggle("show-overlay");
 };
 
+// toggle del menu, mostrar y ocultar
+
 const toggleMenu = () => {
   menuList.classList.toggle("open-menu");
   if (cart.classList.contains("open-cart")) {
@@ -169,6 +184,8 @@ const toggleMenu = () => {
   }
   overlay.classList.toggle("show-overlay");
 };
+
+// toggle del user, iniciar sesion y registrarse
 
 const toggleUser = () => {
   userMenu.classList.toggle("open-user");
@@ -184,6 +201,7 @@ const toggleUser = () => {
   overlay.classList.toggle("show-overlay");
 };
 
+// cerrar las ventanas al scrollear
 const closeOnScroll = () => {
   if (
     !menuList.classList.contains("open-menu") &&
@@ -198,6 +216,7 @@ const closeOnScroll = () => {
   overlay.classList.remove("show-overlay");
 };
 
+// cerrar las ventanas al dar click al overlay
 const closeOnOverlayClick = () => {
   menuList.classList.remove("open-menu");
   cart.classList.remove("open-cart");
@@ -205,12 +224,141 @@ const closeOnOverlayClick = () => {
   overlay.classList.remove("show-overlay");
 };
 
+// cerrar el menu al darle a alguna seccion
 const closeOnClickMenu = () => {
   menuList.classList.remove("open-menu");
   cart.classList.remove("open-cart");
   userMenu.classList.remove("open-user");
   overlay.classList.remove("show-overlay");
 };
+
+// renderizar la card en el carrito
+const renderCardProduct = (card) => {
+  const { cardImg, price, name, id, quantity } = card;
+  return `
+<div class="cart-item">
+            <img src=${cardImg} alt="imagen" /> 
+
+          <div class="item-info">
+              <h3 class="item-title">${name}</h3>
+              <p class="item-price">${price}</p>
+            </div>
+            <div class="item-handler">
+              <span class="quantity-handler down" data-id= ${id}>-</span>
+              <span class="item-quantity">${quantity}</span>
+              <span class="quantity-handler up" data-id= ${id}>+</span>
+            </div>
+          </div> `;
+};
+
+// renderizar la card anterior o mostrar un mensaje
+// si el carro esta vacio.
+const renderCart = () => {
+  if (!carrito.length) {
+    cardContainer.innerHTML = `<p>No hay productos en el carrito.</p>`;
+    return;
+  }
+  cardContainer.innerHTML = carrito.map(renderCardProduct).join("");
+};
+
+// parte logica del precio total del carrito
+const getCardTotal = () => {
+  return carrito.reduce((acc, cur) => {
+    return acc + Number(cur.price) * cur.quantity;
+  }, 0);
+};
+
+// renderizar el total en el carrito
+const showTotal = () => {
+  priceTotal.innerHTML = `${getCardTotal().toFixed(2)}`;
+};
+
+// renderizar cantidad de productos en la burbuja del carrito
+const renderCartBubble = () => {
+  bubbleCart.textContent = carrito.reduce((acc, cur) => {
+    return acc + cur.quantity;
+  }, 0);
+};
+
+// deshabilitar boton si no hay nada en el carrito (parte visual)
+const disableBtn = (btn) => {
+  if (!carrito.length) {
+    btn.classList.add("disabled");
+  } else {
+    btn.classList.remove("disabled");
+  }
+};
+
+// Checa el estado del carrito
+const checkCartState = () => {
+  saveLocalStorage(carrito);
+  renderCart();
+  showTotal();
+  disableBtn(btnBuyCart);
+  disableBtn(btnCleanCart);
+  renderCartBubble();
+};
+
+// agregar al carrito productos
+const addToCart = (e) => {
+  if (!e.target.classList.contains("btn-add")) {
+    return;
+  }
+  const { id, name, price, cardImg } = e.target.dataset;
+
+  const product = productData(id, name, price, cardImg);
+
+  if (isExistingCardProduct(product)) {
+    addUnitToProduct(product);
+    showSuccessModal("Se agregó una unidad del producto al carrito");
+  } else {
+    createCardProduct(product);
+    showSuccessModal("El producto ha sido agregado al carrito");
+  }
+
+  checkCartState();
+};
+
+// pasar la data del producto a un objeto
+const productData = (id, name, price, cardImg) => {
+  return { id, name, price, cardImg };
+};
+
+// si existe el producto en el carrito
+const isExistingCardProduct = (product) => {
+  return carrito.find((item) => {
+    return item.id === product.id;
+  });
+};
+
+// agregar otra unidad del mismo producto si esta en el carrito
+// o agregar el producto directamente
+const addUnitToProduct = (product) => {
+  carrito = carrito.map((cartProduct) => {
+    return cartProduct.id === product.id
+      ? { ...cartProduct, quantity: cartProduct.quantity + 1 }
+      : cartProduct;
+  });
+};
+
+const showSuccessModal = (msg) => {
+  successModal.classList.add("active-modal");
+  successModal.textContent = msg;
+  setTimeout(() => {
+    successModal.classList.remove("active-modal");
+  }, 1500);
+};
+
+const createCardProduct = (product) => {
+  carrito = [
+    ...carrito,
+    {
+      ...product,
+      quantity: 1,
+    },
+  ];
+};
+
 const init = () => {
   renderProducts();
   btnAddMore.addEventListener("click", addMoreProductsBtn);
@@ -221,6 +369,12 @@ const init = () => {
   window.addEventListener("scroll", closeOnScroll);
   overlay.addEventListener("click", closeOnOverlayClick);
   menuList.addEventListener("click", closeOnClickMenu);
+  document.addEventListener("DOMContentLoaded", renderCart);
+  document.addEventListener("DOMContentLoaded", showTotal);
+  renderCartBubble();
+  disableBtn(btnBuyCart);
+  disableBtn(btnCleanCart);
+  productsContainer.addEventListener("click", addToCart);
 };
 
 init();
