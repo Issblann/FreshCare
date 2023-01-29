6f2d44ce5b8b4a309a0dfcecc24c7ee3db6000f3
issblann <anaisabelpp@outlook.com>
2023-01-29 14:07:27 -0500

    agrego toda la zona de login y register con responsive, validacion de formulario y carrusel de imagenes animado

---
+++ assets/Login-register/login-register.js
@@ -0,0 +1,158 @@
+// Validacion de formularios
+// form
+const form = document.querySelector(".form");
+// padre de los input
+const formInput = document.querySelector(".form__input");
+// Input del email
+const inputEmail = document.querySelector(".form__input--email");
+// Input de la contraseña
+const inputPassword = document.querySelector(".form__input--password");
+// Input de nombre
+const inputName = document.querySelector(".form__input--name");
+
+// check del email
+const checkEmail = () => {
+  let valid = false;
+
+  const emailValue = inputEmail.value.trim();
+  if (isEmpty(emailValue)) {
+    showError(inputEmail, "Este campo es obligatorio");
+  } else if (!isValidEmail(emailValue)) {
+    showError(inputEmail, "Ingrese un email valido");
+  } else {
+    showSuccess(inputEmail);
+    valid = true;
+  }
+  return valid;
+};
+
+// check de la contraseña
+const checkPassword = () => {
+  let valid = false;
+
+  const passValue = inputPassword.value.trim();
+
+  if (isEmpty(passValue)) {
+    showError(inputPassword, "Este campo es obligatorio");
+  } else if (!isSecurePassword(passValue)) {
+    showError(
+      inputPassword,
+      "La contraseña debe contener al menos 8 caracteres, una mayúscula, una minuscula y un simbolo"
+    );
+  } else {
+    showSuccess(inputPassword);
+    valid = true;
+  }
+
+  return valid;
+};
+
+const checkUsername = () => {
+  let valid = false;
+
+  const usernameValue = inputName.value.trim();
+
+  if (isEmpty(usernameValue)) {
+    showError(inputName, "Este campo es obligatorio");
+  } else {
+    showSuccess(inputName);
+    valid = true;
+  }
+
+  return valid;
+};
+
+// si esta vacio:
+const isEmpty = (value) => value === "";
+
+// si es valido el email
+const isValidEmail = (email) => {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+};
+
+// si es segura la contraseña
+const isSecurePassword = (pass) => {
+  const re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,}$/;
+  return re.test(pass);
+};
+
+// funcion de error a mostrar en los formularios
+showError = (input, message) => {
+  const saveInput = input.parentElement;
+  saveInput.classList.add("error");
+  saveInput.classList.remove("success");
+
+  const mensajeError = saveInput.querySelector("small");
+
+  mensajeError.textContent = message;
+};
+
+// funcion de valido a mostrar en los formularios
+showSuccess = (input) => {
+  const saveInput = input.parentElement;
+  saveInput.classList.remove("error");
+  saveInput.classList.add("success");
+
+  const mensajeError = saveInput.querySelector("small");
+
+  mensajeError.textContent = "";
+};
+
+// debounce
+
+const debounce = (fn, delay = 500) => {
+  let timeoutId;
+
+  return (...args) => {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      fn.apply(null, args);
+    }, delay);
+  };
+};
+
+// el escuchador del submit
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let isEmailValid = checkEmail();
+  let isPasswordValid = checkPassword();
+  let isUsernameValid = checkUsername();
+
+  let isFormValid = isEmailValid & isPasswordValid & isUsernameValid;
+  if (isFormValid) {
+    form.submit();
+  }
+});
+
+// el escuchador del input para el debounce
+
+form.addEventListener(
+  "input",
+  debounce((e) => {
+    switch (e.target.id) {
+      case "name":
+        checkUsername();
+        break;
+      case "email":
+        checkEmail();
+        break;
+
+      case "password":
+        checkPassword();
+        break;
+    }
+  })
+);
+
+// la logica para arrastrar las imagenes una a una
+let counter = 2;
+setInterval(function () {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+  if (counter > 4) {
+    counter = 1;
+  }
+}, 1500);
